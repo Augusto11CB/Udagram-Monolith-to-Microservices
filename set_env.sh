f7f31feb8465a500055b15aab92f55f261f4cf80
AugustoCalado <augusto.calado11@gmail.com>
2021-01-31 11:08:18 -0300

    C3 Project - Screenshots

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRESS_USERNAME=udagramaugdev
-export POSTGRESS_PASSWORD=augusto11
-export POSTGRESS_HOST=udagramaugdev.comj6e1l4su5.sa-east-1.rds.amazonaws.com
-export POSTGRESS_DATABASE=udagramaugdev
-export AWS_MEDIA_BUCKET=aug-udagram-dev
-export AWS_REGION=sa-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=Blght6heUgevJNoXcP3oRQQ59TU6XPuc
-export URL=http://localhost:8100
+export POSTGRESS_USERNAME=NOT
+export POSTGRESS_PASSWORD=NOT
+export POSTGRESS_HOST=NOT
+export POSTGRESS_DATABASE=NOT
+export AWS_MEDIA_BUCKET=NOT
+export AWS_REGION=NOT
+export AWS_PROFILE=NOT
+export JWT_SECRET=NOT
+export URL=NOT
